fddd0c724312bed3cece49b972efcf106740a25f
Hanif Galuh <hnfglh@gmail.com>
2024-01-13 08:20:23 +0700

    create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,9 +14,22 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'staffa',
+            'email' => 'staffa@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        User::create([
+            'name' => 'staffb',
+            'email' => 'staffb@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        User::create([
+            'name' => 'staffc',
+            'email' => 'staffc@gmail.com',
+            'password' => bcrypt('password')
+        ]);
     }
 }
